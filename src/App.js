08942899dc5d25de91fb5ed360ea2565c7d5f837
ryang <r.gluger23@gmail.com>
2019-10-25 23:29:38 +0100

    first commit of all files l-1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/navbar';
+import BookList from './components/booklist';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/booklist.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+    
+    render() { 
+        return ( 
+            <div className="book-list">
+                <ul>
+                    <li>The Way Of Kings</li>
+                    <li>The Name of the Wind</li>
+                    <li>The Final Empire</li>
+                </ul>
+            </div>
+         );
+    }
+}
+ 
+export default BookList;
