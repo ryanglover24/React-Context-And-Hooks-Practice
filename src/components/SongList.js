56de1a3ce9cc4b4d74d060cd9c93ff4f6ad8ec9c
ryang <r.gluger23@gmail.com>
2019-10-26 14:17:38 +0100

    react hooks useState and uuid generate random id

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uuid from 'uuid/v1';
 
 const SongList = () => {
+    //created state inside a functional Component and destructed to be able to access it songs is array of data
+    const [songs, setSongs] = useState([
+        { title: 'almost home', id: 1},
+        { title: 'memory gospel', id: 2},
+        { title: 'this wild darkness', id: 3},
+    ]);
+    
+    const addSong = () => {
+        setSongs([...songs, { title: 'new song', id: uuid()}])
+    }
 
     return ( 
         <div className="song-list">
             <ul>
-                <li>This wild Darkness</li>
-                <li>memory gospel</li>
+                {songs.map((song) => {
+                    return (
+                        <li key={song.id}>{song.title}</li>
+                    )
+                })}
             </ul>
+            <button onClick={addSong}>Add A Song</button>
         </div>
      );
 }
