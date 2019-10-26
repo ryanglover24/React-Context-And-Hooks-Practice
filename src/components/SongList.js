7a0b62c9a40ce3f00d549c6ac9a18c5c98709a72
ryang <r.gluger23@gmail.com>
2019-10-26 13:59:40 +0100

    setting up app for learning hooks

---
+++ src/components/SongList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SongList = () => {
+
+    return ( 
+        <div className="song-list">
+            <ul>
+                <li>This wild Darkness</li>
+                <li>memory gospel</li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default SongList;
